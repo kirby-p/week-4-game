11e01d9611f8fdd2b248f31f3f0b736aa38d3043
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-04-27 18:02:35 -0400

    User choice correctly returns

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,28 @@
-$( document ).ready(function() {
-
-
 window.onload = function (){
-	$('.charCard').click(game.choose);
+	$('.charCard').on('click', game.choose);
+	// $('#grievous').on('click', game.choose);
+	// $('#vader').on('click', game.choose);
+	// $('#rey').on('click', game.choose);
+
 	$('.charCard').hover(function(){
 		$(this).animate({borderWidth: 5}, 'fast');
 	}, function(){
 		$(this).animate({borderWidth: 0}, 'fast');
-	}
-);
+	});
 };
 
 
 var binks = {
 	health: 75,
-	attack: 15
+	attack: 15,
+	counter: 5
 };
 
 
 var game = {
 	choose:function() {
+		var id = this.id;
+		console.log(id);
 	}
 };
 
-})
