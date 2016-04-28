90172f3143032faa0a91a645c561ba6f6803b78e
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-04-28 01:40:19 -0400

    User choice as chosen character, others as enemies to select

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,8 +21,25 @@ var binks = {
 
 var game = {
 	choose:function() {
-		var id = this.id;
-		console.log(id);
+		$('#chooseTitle').replaceWith('<h2>Your Character</h2>');
+		var playerID = this.id;
+		if(playerID == 'binks'){
+			$('#chooseTitle').replaceWith($('#binks'));
+			$('#chooseEnemy').replaceWith($('#grievous'), $('#vader'), $('#rey'));
+		}
+		else if(playerID == 'grievous'){
+			$('#chooseTitle').replaceWith($('#grievous'));
+			$('#chooseEnemy').replaceWith($('#binks'), $('#vader'), $('#rey'));		
+		}
+		else if(playerID == 'vader'){
+			$('#chooseTitle').replaceWith($('#vader'));
+			$('#chooseEnemy').replaceWith($('#binks'), $('#grievous'), $('#rey'));		
+		}
+		else if(playerID == 'rey'){
+			$('#chooseTitle').replaceWith($('#rey'));
+			$('#chooseEnemy').replaceWith($('#binks'), $('#grievous'), $('#vader'));		
+		}
+
 	}
 };
 
